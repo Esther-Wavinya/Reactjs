05241c0b850c2c93d9cdfbe2003d04f5beaaf003
Esther-Wavinya <estherwavinya80@gmail..com>
2023-03-22 23:13:49 +0300

    jsx

---
+++ jsx/src/Hello.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+class Hello extends React.Component {
+    render() {
+      return <h1>Hello There!!!</h1>
+    }
+  }
---
+++ jsx/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Hello />
+        <NumPicker />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
